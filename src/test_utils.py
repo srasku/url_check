f0fb90245c6dd025e65d4636a81ad0dc99a782fd
Stephen Rasku <mercurial@srasku.net>
2021-04-23 10:36:08 -0700

    Handle missing port
    Also added test for non-fully qualified domain name.

--- src/test_utils.py
+++ src/test_utils.py
@@ -5,6 +5,9 @@ import utils
 class TestGetHostAndPort(unittest.TestCase):
     def test_valid_case(self):
         self.host_port_tester('www.some-domain.com:8129', 'www.some-domain.com', 8129)
+        self.host_port_tester('www.another-domain.com', 'www.another-domain.com', 80)
+        self.host_port_tester('host-only:8912', 'host-only', 8912)
+
     def host_port_tester(self, param, expected_domain, expected_port):
         tuple = utils.get_host_and_port(param)
         self.assertEqual(len(tuple), 2)
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,12 @@
 """
 Returns tuple representing host and port based on the provided `param`.
 The format of this string is expected to be `host:port`.  For example,
-a valid string would be 'www.some-domain.com:8080'.
+a valid string would be 'www.some-domain.com:8080'.  If the port is 
+omitted, it is assumed to be 80.
 """
 def get_host_and_port(param):
-    host, port = param.split(':')
-    port = int(port)
+    tuple = param.split(':')
+    host = tuple[0]
+    port = int(tuple[1]) if len(tuple) > 1 else 80
 
     return host, port
