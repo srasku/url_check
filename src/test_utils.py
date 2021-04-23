aafbb796ad532988882cd5ef7864b88105256f13
Stephen Rasku <mercurial@srasku.net>
2021-04-23 10:33:32 -0700

    Add host_port_tester() helper function
    This will simplify adding test cases.

--- src/test_utils.py
+++ src/test_utils.py
@@ -4,7 +4,9 @@ import utils
 
 class TestGetHostAndPort(unittest.TestCase):
     def test_valid_case(self):
-        tuple = utils.get_host_and_port('www.some-domain.com:8129')
+        self.host_port_tester('www.some-domain.com:8129', 'www.some-domain.com', 8129)
+    def host_port_tester(self, param, expected_domain, expected_port):
+        tuple = utils.get_host_and_port(param)
         self.assertEqual(len(tuple), 2)
-        self.assertEqual(tuple[0], 'www.some-domain.com')
-        self.assertEqual(tuple[1], 8129)
+        self.assertEqual(tuple[0], expected_domain)
+        self.assertEqual(tuple[1], expected_port)
