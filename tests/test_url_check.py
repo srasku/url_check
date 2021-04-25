14a23eb49adff7c7df8e2eeae3a42a4c12aad6b6
Stephen Rasku <mercurial@srasku.net>
2021-04-25 12:52:44 -0700

    Add url_check_tester()
    This is a helper used to reduce code duplication.

--- tests/test_url_check.py
+++ tests/test_url_check.py
@@ -7,10 +7,15 @@ def test_url_check(client):
     """
     Test blacklist endpoint `urlinfo/1/<host>:<port>`
     """
-    response = client.get('urlinfo/1/localhost:99')
-    assert response.status_code == 204
-    assert response.data == b''
+    url_check_tester(client, 'urlinfo/1/localhost:99', 204)
+    url_check_tester(client, 'urlinfo/1/bad-host.com:8899', 403)
 
-    response = client.get('urlinfo/1/bad-host.com:8899')
-    assert response.status_code == 403
+
+def url_check_tester(client, url, status_code):
+    """
+    Helper function to test blacklist endpoint.  This reduces the ammount of
+    code duplicated for each individual test above.
+    """
+    response = client.get(url)
+    assert response.status_code == status_code
     assert response.data == b''
